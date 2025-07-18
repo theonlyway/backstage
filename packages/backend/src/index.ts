e4eaeca7aa28909938c816a3e3e879e8c425065a
Anthony <theonlyway@users.noreply.github.com>
2025-07-18 00:30:42 +0000

    added scaffolder plugin for ado

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -18,6 +18,7 @@ backend.add(import('@backstage/plugin-app-backend'));
 backend.add(import('@backstage/plugin-proxy-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-azure'));
 backend.add(import('@backstage/plugin-techdocs-backend'));
 
 // auth plugin
