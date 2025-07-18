62c3ff8afc08fb6e7f5aecbcc7df121bf7f13a77
Anthony <theonlyway@users.noreply.github.com>
2025-07-18 04:13:47 +0000

    added extra plugin

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -31,6 +31,7 @@ backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
 backend.add(import('@backstage/plugin-catalog-backend-module-msgraph'));
 backend.add(import('@backstage/plugin-catalog-backend-module-azure'));
 backend.add(
